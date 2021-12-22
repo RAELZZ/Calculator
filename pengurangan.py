ab8b2f50395e212041cd2a689c9119d237564006
prk <rafaeljeremy1402@gmial.com>
2021-12-22 01:08:59 -0800

    menambahkan branch perkalian dan fungsi perkalian

---
+++ pengurangan.py
@@ -0,0 +1,8 @@
+def pengurangan(a,b):
+	total = a-b
+	return total
+
+def main():
+	print (penambahan (10,5))
+	print (pengurangan(10,5))
+main()
---
+++ perkalian.py
@@ -0,0 +1,9 @@
+def perkalian(a,b):
+	total = a*b
+	return total
+def main():
+	print (perkalian(10,5))
+	print (penambahan(10,5))
+	print (pengurangan(10,5))
+
+main()
